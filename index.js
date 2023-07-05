dcc20e4045d50cd48a661c20f6a38a2cc977eba0
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-05 09:35:36 +0530

    make it default port 3000 fixed for now

--- index.js
+++ index.js
@@ -198,5 +198,6 @@ app.get('/api/search', (req, res) => {
   
 
 // Start the server
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
